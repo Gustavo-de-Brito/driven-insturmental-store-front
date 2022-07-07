90f508c191bf29e2dedfc34acaf806c6562e7b4d
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-07-07 11:16:06 -0300

    adiciona menu de filtragem lateral

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { useLocation } from "react-router-dom";
+import SideMenu from "./SideMenu";
 import { BsCart2 } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 function Navbar() {
+  const canShowNavbar= () => {
+    return currentRoute === "/" || currentRoute === "/cart" || currentRoute === "/checkout";
+  }
+
+  const location = useLocation();
+  const currentRoute = location.pathname;
+  const showNavbar = useState(canShowNavbar);
+  const [ showFilterMenu, setShowFilterMenu ] = useState(false);
+
   return (
-    <NavigationBar>
-      <GiHamburgerMenu style={{ color:"#FFFFFF", fontSize: "40px" }} />
+    <NavigationBar showNavbar={ showNavbar }>
+      <GiHamburgerMenu onClick={ () => setShowFilterMenu(true) } style={{ color:"#FFFFFF", fontSize: "40px" }} />
       <h1>DRIVEN <span>Instrumental</span></h1>
       <BsCart2 style={{ color:"#FFFFFF", fontSize: "40px" }} />
+      {
+        showFilterMenu ? <SideMenu setShowFilterMenu={ setShowFilterMenu } /> : <></>
+      }
     </NavigationBar>
   );
 }
 
 const NavigationBar = styled.nav`
-  display: flex;
+  display: ${({ showNavbar }) => showNavbar ? "flex": "none" };
   justify-content: space-between;
   align-items: center;
   width: 100%;
