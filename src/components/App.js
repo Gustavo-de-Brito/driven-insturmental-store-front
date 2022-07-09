e706242df314a5bf7636b56f0b9571f7a365c62e
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-07-09 01:57:38 -0300

    implementation of sign in/up pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { useContext } from "react";
+
 import HomeView from "./Home/HomeView";
 import GlobalStyle from "./theme/globalStyle";
+import Login from "./Login";
+import Cadastro from "./Cadastro";
+import UserContext from "./UserContext";
 
 function App() {
+  const [userData, setUserData] = useState(null);
+  const [userName, setUserName] = useState("");
+
   return (
     <>
+      <UserContext.Provider value={{userData, setUserData, userName, setUserName}}>
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <HomeView /> } /> 
+          <Route path="/" element={ <HomeView /> } />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
         </Routes>
       </BrowserRouter>
+      </UserContext.Provider>
     </>
   );
 }
